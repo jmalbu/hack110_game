a5265889ebbcd1568430d13e9f24aab07176294c
Mary Leigh Cook <mcook2@unc.edu>
2023-04-22 03:44:22 -0400

    yes

---
+++ constants.py
@@ -0,0 +1,16 @@
+import random
+
+
+HERO = "assets/hero.com.png"
+HERO_SPEED = 1
+BACKGROUND = "assets/spooky_back.png"
+ENEMY = "assets/enemy.png"
+BULLET_SPEED = 5
+TIMED_BULLET = random.randint(1000, 3000)
+BULLET = "assets/bullet_pump.png"
+
+BACKGROUND_HEIGHT = 600 
+BACKGROUND_WIDTH = 900
+
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
--- sprites_enemy.py
+++ sprites_enemy.py
@@ -1,5 +1,7 @@
+from constants import *
 import pygame
-from pygame.locals import K_DOWN, K_LEFT, K_RIGHT, K_UP, RLEACCEL
+from pygame.locals import RLEACCEL
+import random
 
 class Enemy(pygame.sprite.Sprite):
     """Evil pumpkin."""
@@ -9,18 +11,33 @@ class Enemy(pygame.sprite.Sprite):
         super(Enemy, self).__init__()
         
         # Create the image for the enemy to be, convert it
-        self.surf = pygame.image.load(enemy.png).convert()
+        self.surf = pygame.image.load(ENEMY.png).convert()
 
         # Remove the black background
         self.surf.set_colorkey((0, 0, 0), RLEACCEL)
 
-class Projectile(object):
-    """Bullets."""
+        # Sets pumpkin to middle of the screen
+        self.rect = self.surf.get_rect(center = (800, BACKGROUND_HEIGHT / 2))
 
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-        self.surf = pygame.image.load(enemy.png).convert()
 
-    def draw(self,win):
-        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
+class Projectile(pygame.sprite.Sprite):
+    """Projectiles aka bullets aka."""
+
+    def __init__(self) -> None:
+        # Super class initialization of the sprite
+        super(Enemy, self).__init__()
+
+        # Create the image for the enemy to be, convert it
+        image_path = BULLET
+        self.surf = pygame.image.load(image_path).convert()
+
+        # Remove the black background
+        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
+
+        # Spawns the bullets from the pumpkin
+        self.rect = self.surf.get_rect(center = (800, BACKGROUND_HEIGHT / 2))
+
+        # Set their speed to the constant from the constants file
+        self.speed = BULLET_SPEED
+
+
